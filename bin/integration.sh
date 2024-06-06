1e3a3d360df2b207bca08b1cbd47fc400c955d66
Jean-Christophe Amiel <jeanchristophe.amiel@gmail.com>
2024-06-06 11:34:29 +0200

    Ajout de nos premiers tests dintegration

--- bin/integration.sh
+++ bin/integration.sh
@@ -6,12 +6,15 @@ nohup node dist/bin/www > dist/output.log 2>&1 &
 server_pid="$!"
 echo "Starter server pid $server_pid"
 
+
+
 echo "Waiting server to be ready..."
 echo -e "GET http://localhost:3000\nHTTP 200" | \
   hurl \
   --retry 60 \
   --retry-interval 1000 \
   --no-output
+
 
 echo "Running Hurl tests"
 hurl --test --parallel integration/*.hurl
